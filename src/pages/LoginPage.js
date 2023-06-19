020c36c6dcab32b675dec28c1917e71cd2d238e2
Juhee Noh <nohjuhee227@gmail.com>
2023-06-20 02:04:54 +0900

    style: 로그인 페이지 레이아웃

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from '../components/header/Header';
+import AuthForm from '../components/authority/AuthForm';
 
 const LoginPage = () => {
-  return <div>Login-Page</div>;
+  const loginSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log('로그인 버튼 클릭!');
+  };
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <AuthForm onSubmit={loginSubmitHandler} btnValue="로그인" />
+      </main>
+    </Fragment>
+  );
 };
 
 export default LoginPage;
