b09643ec0fb7db6786235719828d39f6ee916319
Juhee Noh <nohjuhee227@gmail.com>
2023-06-20 06:39:24 +0900

    feat. 할 일 추가하기

--- src/components/todoList/AddTodo.js
+++ src/components/todoList/AddTodo.js
@@ -4,11 +4,22 @@ import Input from '../UI/Input';
 import Button from '../UI/Button';
 
 import classes from './AddTodo.module.css';
-const AddTodo = () => {
-  const TodoInputRef = useRef();
+const AddTodo = ({ onAdd }) => {
+  const todoInputRef = useRef();
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: Math.random().toString(),
+      isChecked: false,
+      todo: todoInputRef.current.value,
+    };
+    onAdd(newItem);
+    todoInputRef.current.value = '';
+  };
   return (
-    <form className={classes.form}>
-      <Input ref={TodoInputRef} id="new-todo-input" label="할 일" type="text" />
+    <form className={classes.form} onSubmit={addTodoHandler}>
+      <Input ref={todoInputRef} id="new-todo-input" label="할 일" type="text" />
       <Button type="submit" id="new-todo-add-button">
         +
       </Button>
--- src/pages/TodoListPage.js
+++ src/pages/TodoListPage.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 import Header from '../components/header/Header';
 import TodoList from '../components/todoList/TodoList';
@@ -8,7 +8,7 @@ import classes from './TodoListPage.module.css';
 import AddTodo from '../components/todoList/AddTodo';
 
 const TodoListPage = () => {
-  const todoList = [
+  const data = [
     {
       id: 'todo1',
       isChecked: false,
@@ -20,12 +20,18 @@ const TodoListPage = () => {
       todo: '해야 할 일 2',
     },
   ];
+  const [todoList, setTodoList] = useState(data);
+  const addTodoListHandler = (item) => {
+    setTodoList((todoList) => {
+      return todoList.concat(item);
+    });
+  };
   return (
     <Fragment>
       <Header />
       <main>
         <Card className={classes.todoListCard}>
-          <AddTodo />
+          <AddTodo onAdd={addTodoListHandler} />
           <TodoList todoList={todoList} />
         </Card>
       </main>
