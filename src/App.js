a8133cf36340542eb0e98f3b2116a833592c555f
Juhee Noh <nohjuhee227@gmail.com>
2023-06-21 16:41:07 +0900

    feat: 토큰 여부에 따른 리다이렉트 처리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
@@ -7,13 +7,23 @@ import Main from './pages/Main';
 import AuthContextProvider from './store/AuthProvider';
 
 function App() {
+  const token = localStorage.getItem('access_token');
   return (
     <AuthContextProvider>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/signup" element={<RegisterPage />} />
-        <Route path="/signin" element={<LoginPage />} />
-        <Route path="/todo" element={<TodoListPage />} />
+        <Route
+          path="/signup"
+          element={token ? <Navigate to="/todo" /> : <RegisterPage />}
+        />
+        <Route
+          path="/signin"
+          element={token ? <Navigate to="/todo" /> : <LoginPage />}
+        />
+        <Route
+          path="/todo"
+          element={token ? <TodoListPage /> : <Navigate to="/signin" />}
+        />
       </Routes>
     </AuthContextProvider>
   );
